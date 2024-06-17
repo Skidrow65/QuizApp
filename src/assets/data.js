4be8a8d80b46387b4cf3329179c5f7a716ed1c37
skidrow <136255106+Skidrow65@users.noreply.github.com>
2024-06-17 21:34:56 +0300

    Update data.js

--- src/assets/data.js
+++ src/assets/data.js
@@ -27,7 +27,7 @@ export const data = [
       question: "who among the following is not a founding father of physics",
       option1: "Isaac Newton",
       option2: "Galileo Galilei",
-      option3: "squidward",
+      option3: "squidward.",
       option4: "Carl max",
       ans: 3,
     },
