adb5b0784dbcf7f1b4d0330ac30a3fe119f9dc5e
skidrow <136255106+Skidrow65@users.noreply.github.com>
2024-10-28 11:51:48 +0300

    Update data.js

--- src/assets/data.js
+++ src/assets/data.js
@@ -4,7 +4,7 @@ export const data = [
       option1: "Like poles repel",
       option2: "Unlike poles attract.",
       option3: "magnetic force is most concentrated at the poles.",
-      option4: "magnets can be made of any material.",
+      option4: "magnets can be made of any material",
       ans: 4,
     },
     {
